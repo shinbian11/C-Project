92288c371673ac8584e1d5fb04a76c3d5eac8fca
shinbian11 <59140182+shinbian11@users.noreply.github.com>
2020-02-24 20:36:33 +0900

    Update qsort.c

--- qsort.c
+++ qsort.c
@@ -2,6 +2,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 int values[] = { 98,23,99,37,16 };
+#define MAX 1000000
+int values[MAX];
 int compare(const void* a, const void* b)
 {
 	return *(int*)a - *(int*)b; //첫번째 원소가 더 크면 양수반환,더 작으면 음수반환, 같으면 0   (오름차순)
@@ -14,12 +16,14 @@ int main()
 
 	printf("\n\n");
 
-	qsort(values, 5, sizeof(int), compare); 
+
+	qsort(values, n, sizeof(int), compare);
 	//정렬할 배열,원소개수,각원소의크기,비교함수
 
 
 	printf("정렬 후 : ");
+
 	for (int i = 0; i < 5; i++)
 		printf("%d ", values[i]);
-	
+
 }
